74c8e310e467b5987683225b22f68825743914f4
Ziliang Zhang <z.zhang-78@student.tudelft.nl>
2025-03-25 22:38:11 +0100

    new name group12

--- agents/group12_agent/group12_agent.py
+++ agents/group12_agent/group12_agent.py
@@ -30,7 +30,7 @@ from tudelft_utilities_logging.ReportToLogger import ReportToLogger
 from .utils.opponent_model import OpponentModel
 
 
-class TemplateAgent(DefaultParty):
+class Group12Agent(DefaultParty):
     """
     Template of a Python geniusweb agent.
     """
